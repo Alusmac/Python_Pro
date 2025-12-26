b7e347094a336db61b1de451833634ccf133f369
Alus <avp3085@gmail.com>
2025-12-26 13:54:33 +0100

    commit

--- Pro_dz_1.py
+++ Pro_dz_1.py
@@ -8,13 +8,10 @@ def calculate_circle_area(radius: float) -> float:
     :param radius: Radius of the circle
     :return: Area of the circle
     """
-    return math.pi * radius**2
+    return math.pi * radius ** 2
 
 
 radius: float = float(input("Enter the radius of the circle: "))
 area: float = calculate_circle_area(radius)
 
 print(f"The area of the circle is: {area:.2f}")
-
-
-
