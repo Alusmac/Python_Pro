fdc6c47a49e62497d557baed14bf59273f7f6eb3
Alus <avp3085@gmail.com>
2026-01-09 12:09:10 +0100

    commit

--- L5/dz5.py
+++ L5/dz5.py
@@ -1,14 +1,11 @@
 class MutableClass:
+    """клас MutableClass, який має методи для динамічного додавання та видалення атрибутів об'єкта
+    """
+
     def add_attribute(self, name: str, value) -> None:
-        """
-        Додає атрибут з ім'ям name та значенням value
-        """
         setattr(self, name, value)
 
     def remove_attribute(self, name: str) -> None:
-        """
-        Видаляє атрибут з ім'ям name
-        """
         if hasattr(self, name):
             delattr(self, name)
         else:
